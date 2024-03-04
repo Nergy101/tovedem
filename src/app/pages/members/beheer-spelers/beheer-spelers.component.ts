bbf4417b8d5c09d099d2083028790317a1418e98
Christian van Dijk <christian.vandijk@akzonobel.com>
2024-03-04 21:47:00 +0100

    add beginning of beheer-spelers

--- src/app/pages/members/beheer-spelers/beheer-spelers.component.ts
+++ src/app/pages/members/beheer-spelers/beheer-spelers.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  WritableSignal,
+  inject,
+  signal,
+} from '@angular/core';
+
+import { MatTableModule } from '@angular/material/table';
+import { PocketbaseService } from '../../../shared/services/pocketbase.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-beheer-spelers',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatIconModule, MatButtonModule],
   templateUrl: './beheer-spelers.component.html',
-  styleUrl: './beheer-spelers.component.scss'
+  styleUrl: './beheer-spelers.component.scss',
 })
-export class BeheerSpelersComponent {
+export class BeheerSpelersComponent implements OnInit {
+  spelers: WritableSignal<any | null> = signal(null);
 
+  client = inject(PocketbaseService).client;
+
+  displayedColumns = ['id', 'naam', 'actions'];
+
+  async ngOnInit(): Promise<void> {
+    const spelers = (await this.client
+      .collection('spelers')
+      .getFullList()) as any[];
+
+    this.spelers.set(spelers);
+  }
+
+  async delete(element: any) {
+    await this.client.collection('spelers').delete(element.id);
+
+    this.spelers.update((x) => x.filter((y: any) => y.id != element.id));
+  }
 }
