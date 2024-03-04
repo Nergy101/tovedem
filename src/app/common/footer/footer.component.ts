a8453e45149d36017927dbea87832a3e01de28a8
Christian van Dijk <christian.vandijk@akzonobel.com>
2024-03-04 21:04:34 +0100

    fix errors

--- src/app/common/footer/footer.component.ts
+++ src/app/common/footer/footer.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CarouselModule],
+  imports: [],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
