3ad1d91a630a9ecb7d5c0ea62dd2cdf43118fe0b
Christian van Dijk <cdijk4@gmail.com>
2024-11-15 23:11:52 +0100

    ✨🐛🧑‍💻 working sint and reservatie mails

--- pb_hooks/mailing.js
+++ pb_hooks/mailing.js
@@ -1,61 +1,105 @@
 module.exports = {
     getMail: (mailName) => {
-        const record = $app.dao().findFirstRecordByData("mails", "naam", mailName)
+        const filter = `naam = '${mailName}'`;
+        const record = $app.dao().findFirstRecordByFilter("mails", filter);
+
+        if (!mailInfo) {
+            throw new Error("Mail template 'sintcommissie' not found");
+        }
+
+        if (record.length > 1) {
+            throw new Error(
+                "Multiple mails found with the same name: " + mailName,
+            );
+        }
+
+        return record;
     },
     getReservatieMailHtml: (mailInfo, reservatie, voorstelling) => {
-
-        const tijdOnly1 = new Date(voorstelling.datum_tijd_1).toLocaleTimeString('nl-NL', {
-            hour: '2-digit',
-            minute: '2-digit'
+        const tijdOnly1 = new Date(
+            voorstelling.get("datum_tijd_1"),
+        ).toLocaleTimeString("nl-NL", {
+            hour: "2-digit",
+            minute: "2-digit",
         });
 
-        const datumOnly1 = new Date(voorstelling.datum_tijd_1).toLocaleDateString('nl-NL', {
-            weekday: 'long',
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric'
+        const datumOnly1 = new Date(
+            voorstelling.get("datum_tijd_1"),
+        ).toLocaleDateString("nl-NL", {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
         });
 
-        const tijdOnly2 = new Date(voorstelling.datum_tijd_2).toLocaleTimeString('nl-NL', {
-            hour: '2-digit',
-            minute: '2-digit'
+        const tijdOnly2 = new Date(
+            voorstelling.get("datum_tijd_2"),
+        ).toLocaleTimeString("nl-NL", {
+            hour: "2-digit",
+            minute: "2-digit",
         });
 
-        const datumOnly2 = new Date(voorstelling.datum_tijd_2).toLocaleDateString('nl-NL', {
-            weekday: 'long',
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric'
+        const datumOnly2 = new Date(
+            voorstelling.get("datum_tijd_2"),
+        ).toLocaleDateString("nl-NL", {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
         });
 
-        let mailHtml = mailInfo.inhoud
+        let mailHtml = mailInfo.get("inhoud");
 
         // replace all placeholders with the actual values
-        mailHtml = mailHtml.replace(/{{reserveerdersNaam}}/g, reservatie.voornaam + " " + reservatie.achternaam)
-        mailHtml = mailHtml.replace(/{{voorstellingsNaam}}/g,)
+        mailHtml = mailHtml.replace(
+            /{reserveerdersNaam}/g,
+            reservatie.get("voornaam") + " " + reservatie.get("achternaam"),
+        );
+        mailHtml = mailHtml.replace(/{voorstellingsNaam}/g, voorstelling.get("titel"));
 
-        mailHtml = mailHtml.replace(/{{aantal1}}/g, reservatie.datum_tijd_1_aantal)
-        mailHtml = mailHtml.replace(/{{datum1}}/g, datumOnly)
-        mailHtml = mailHtml.replace(/{{tijd1}}/g, tijdOnly1)
-        mailHtml = mailHtml.replace(/{{islid1}}/g, reservatie.is_lid_van_vereniging ? "Ja" : "Nee")
-        mailHtml = mailHtml.replace(/{{isvriend1}}/g, reservatie.is_vriend_van_tovedem ? "Ja" : "Nee")
+        mailHtml = mailHtml.replace(
+            /{aantal1}/g,
+            reservatie.get("datum_tijd_1_aantal"),
+        );
+        mailHtml = mailHtml.replace(/{datum1}/g, datumOnly);
+        mailHtml = mailHtml.replace(/{tijd1}/g, tijdOnly1);
+        mailHtml = mailHtml.replace(
+            /{islid1}/g,
+            reservatie.get("is_lid_van_vereniging") ? "Ja" : "Nee",
+        );
+        mailHtml = mailHtml.replace(
+            /{isvriend1}/g,
+            reservatie.get("is_vriend_van_tovedem") ? "Ja" : "Nee",
+        );
 
-        mailHtml = mailHtml.replace(/{{aantal2}}/g, reservatie.datum_tijd_2_aantal)
-        mailHtml = mailHtml.replace(/{{datum2}}/g, datumOnly2)
-        mailHtml = mailHtml.replace(/{{tijd2}}/g, tijdOnly2)
-        mailHtml = mailHtml.replace(/{{islid2}}/g, reservatie.is_lid_van_vereniging ? "Ja" : "Nee")
-        mailHtml = mailHtml.replace(/{{isvriend2}}/g, reservatie.is_vriend_van_tovedem ? "Ja" : "Nee")
+        mailHtml = mailHtml.replace(
+            /{aantal2}/g,
+            reservatie.get("datum_tijd_2_aantal"),
+        );
+        mailHtml = mailHtml.replace(/{datum2}/g, datumOnly2);
+        mailHtml = mailHtml.replace(/{tijd2}/g, tijdOnly2);
+        mailHtml = mailHtml.replace(
+            /{islid2}/g,
+            reservatie.get("is_lid_van_vereniging") ? "Ja" : "Nee",
+        );
+        mailHtml = mailHtml.replace(
+            /{isvriend2}/g,
+            reservatie.get("is_vriend_van_tovedem") ? "Ja" : "Nee",
+        );
 
-        mailHtml = mailHtml.replace(/{{reserveringid}}/g, reservatie.id)
-        mailHtml = mailHtml.replace(/{{guid}}/g, reservatie.guid)
-
-        return mailHtml
-    },
-    getSintcommissieMailHtml: (mailInfo, verzoek) => {
-        let mailHtml = mailInfo.inhoud
-
-        mailHtml = mailHtml.replace(/{{naam}}/g, verzoek.name)
+        mailHtml = mailHtml.replace(/{reserveringid}/g, reservatie.get("id"));
+        mailHtml = mailHtml.replace(/{guid}/g, reservatie.get("guid"));
 
         return mailHtml;
-    }
-}
+    },
+    getSintcommissieMailHtml: (mailInfo, verzoek) => {
+        $app.logger().info("mailinfo", JSON.stringify(mailInfo));
+        $app.logger().info("verzoek", JSON.stringify(verzoek));
+
+        let mailHtml = mailInfo.get("inhoud");
+
+        mailHtml = mailHtml.replace(/{naam}/g, verzoek.get("name"));
+
+        return mailHtml;
+    },
+};
--- pb_hooks/mails.pb.js
+++ pb_hooks/mails.pb.js
@@ -1,85 +1,58 @@
-
-
 /* Send an email after somebody did a reservation */
 onRecordAfterCreateRequest((e) => {
-    const mailing = require(`${__hooks}/mailing.js`)
+    const mailing = require(`${__hooks}/mailing.js`);
 
-    const reservatie = e.record
-    $app.dao().expandRecord(e.record, ["voorstelling"], null)
-    const voorstelling = e.record.expandedOne("voorstelling")
+    const reservatie = e.record;
+    $app.dao().expandRecord(e.record, ["voorstelling"], null);
+    const voorstelling = e.record.expandedOne("voorstelling");
 
-    const mailInfo = mailing.getMail("reservatie_confirmatie")
+    const mailInfo = mailing.getMail("reservatie_confirmatie");
 
-    if (!!mailInfo) {
-        throw new Error("Mail template 'reservatie_confirmatie' not found")
-    }
-
-    const filledMailTemplate = mailing.getReservatieMailHtml(mailInfo, reservatie, voorstelling)
+    const filledMailTemplate = mailing.getReservatieMailHtml(
+        mailInfo,
+        reservatie,
+        voorstelling,
+    );
 
     const message = new MailerMessage({
         from: {
             address: $app.settings().meta.senderAddress,
             name: $app.settings().meta.senderName,
         },
-        to: [{ address: e.record.email() }],
-        subject: mailInfo.onderwerp,
-        html: filledMailTemplate
-    })
+        to: [{ address: e.record.get("email") }],
+        subject: mailInfo.get("onderwerp"),
+        html: filledMailTemplate,
+    });
 
-    $app.newMailClient().send(message)
-}, "reserveringen")
+    $app.newMailClient().send(message);
+}, "reserveringen");
 
 /* Send an email after somebody did a sinterklaas verzoek */
 onRecordAfterCreateRequest((e) => {
-    const mailing = require(`${__hooks}/mailing.js`)
+    const mailing = require(`${__hooks}/mailing.js`);
 
-    const verzoek = e.record
+    const verzoek = e.record;
 
-    const mailInfo = mailing.getMail("sintcommissie")
+    const mailInfo = mailing.getMail("sintcommissie");
 
-    if (!!mailInfo) {
-        throw new Error("Mail template 'sintcommissie' not found")
-    }
+    const filledMailTemplate = mailing.getSintcommissieMailHtml(
+        mailInfo,
+        verzoek,
+    );
 
-    const filledMailTemplate = mailing.getSintcommissieMailHtml(mailInfo, verzoek)
+    const recipient = e.record.get("email");
 
-    const message = new MailerMessage({
-        from: {
-            address: $app.settings().meta.senderAddress,
-            name: $app.settings().meta.senderName,
-        },
-        to: [{ address: e.record.email() }],
-        subject: mailInfo.onderwerp,
-        html: filledMailTemplate
-    })
-
-    $app.newMailClient().send(message)
-}, "sinterklaas_verzoeken")
-
-
-/* Send an email after somebody did a sinterklaas verzoek */
-onRecordAfterCreateRequest((e) => {
-    const mailing = require(`${__hooks}/mailing.js`)
-
-    const verzoek = e.record
-
-    const mailInfo = mailing.getMail("sintcommissie")
-
-    if (!!mailInfo) {
-        throw new Error("Mail template 'sintcommissie' not found")
-    }
-
-    const filledMailTemplate = mailing.getSintcommissieMailHtml(mailInfo, verzoek)
+    $app.logger().info("recipient", JSON.stringify(recipient));
 
     const message = new MailerMessage({
         from: {
             address: $app.settings().meta.senderAddress,
             name: $app.settings().meta.senderName,
         },
-        to: [{ address: e.record.email() }],
-        subject: mailInfo.onderwerp,
-        html: filledMailTemplate
-    })
+        to: [{ address: recipient }],
+        subject: mailInfo.get("onderwerp"),
+        html: filledMailTemplate,
+    });
 
-    $app.newMailClient().send(message)
-}, "sinterklaas_verzoeken")
+    $app.newMailClient().send(message);
+}, "sinterklaas_verzoeken");
