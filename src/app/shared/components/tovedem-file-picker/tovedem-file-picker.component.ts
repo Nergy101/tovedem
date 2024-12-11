227e01db97d92c8d2e925702ea6f83d5a3167e18
Christian van Dijk <cdijk4@gmail.com>
2024-12-11 19:23:12 +0100

    upgrade to angular 18

--- src/app/shared/components/tovedem-file-picker/tovedem-file-picker.component.ts
+++ src/app/shared/components/tovedem-file-picker/tovedem-file-picker.component.ts
@@ -2,12 +2,16 @@ import { Component, EventEmitter, Output } from '@angular/core';
 
 import { FilePickerModule, FilePreviewModel } from 'ngx-awesome-uploader';
 import { PocketBaseFilePickerAdapter } from './pocketbase-file-picker-adapter';
-import { HttpClientModule } from '@angular/common/http';
+import {} from '@angular/common/http';
 
 @Component({
   selector: 'app-tovedem-file-picker',
   standalone: true,
-  imports: [FilePickerModule, HttpClientModule],
+  imports: [FilePickerModule, 
+// TODO: `HttpClientModule` should not be imported into a component directly.
+// Please refactor the code to add `provideHttpClient()` call to the provider list in the
+// application bootstrap logic and remove the `HttpClientModule` import from this component.
+HttpClientModule],
   templateUrl: './tovedem-file-picker.component.html',
   styleUrl: './tovedem-file-picker.component.scss',
 })
