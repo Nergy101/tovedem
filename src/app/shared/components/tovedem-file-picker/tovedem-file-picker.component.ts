c8497688578942a65dfdc37293b43dbcfa7a1317
Christian van Dijk <cdijk4@gmail.com>
2024-12-11 19:32:33 +0100

    upgraded to angular 18

--- src/app/shared/components/tovedem-file-picker/tovedem-file-picker.component.ts
+++ src/app/shared/components/tovedem-file-picker/tovedem-file-picker.component.ts
@@ -7,11 +7,7 @@ import {} from '@angular/common/http';
 @Component({
   selector: 'app-tovedem-file-picker',
   standalone: true,
-  imports: [FilePickerModule, 
-// TODO: `HttpClientModule` should not be imported into a component directly.
-// Please refactor the code to add `provideHttpClient()` call to the provider list in the
-// application bootstrap logic and remove the `HttpClientModule` import from this component.
-HttpClientModule],
+  imports: [FilePickerModule],
   templateUrl: './tovedem-file-picker.component.html',
   styleUrl: './tovedem-file-picker.component.scss',
 })
