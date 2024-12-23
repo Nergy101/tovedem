bacbc261c507cc70350ff991aa899791d2d5ff29
Christian van Dijk <cdijk4@gmail.com>
2024-12-23 23:02:17 +0100

    🔍️ add canonical urls

--- src/app/shared/services/seo.service.ts
+++ src/app/shared/services/seo.service.ts
@@ -1,36 +1,47 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { inject, Injectable } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class SeoService {
+  router = inject(Router);
+  titleService = inject(Title);
+  metaService = inject(Meta);
 
-    titleService = inject(Title);
-    metaService = inject(Meta);
+  update(title: string): void {
+    this.updateTitle(title);
+    this.updateMetaTags([{ name: 'description', content: title }]);
+    this.updateCanonicalUrl(this.router.url);
+  }
 
-    update(title: string): void {
-        this.updateTitle(title);
-        this.updateMetaTags([{ name: 'description', content: title }]);
+  updateStructuredData(data: any): void {
+    const structuredData = this.metaService.getTag('json+ld');
+    if (!structuredData) {
+      this.metaService.addTag({ name: 'json+ld', content: JSON.stringify(data) });
+    } else {
+      this.metaService.updateTag({ name: 'json+ld', content: JSON.stringify(data) });
     }
+  }
 
-    updateCustom(title: string, metaTags: { name: string, content: string }[]): void {
-        this.updateTitle(title);
-        this.updateMetaTags(metaTags);
-    }
+  private updateTitle(title: string): void {
+    this.titleService.setTitle(title);
+  }
 
-    updateStructuredData(data: any): void {
-        this.metaService.addTag({ name: 'json+ld', content: JSON.stringify(data) });
+  private updateCanonicalUrl(url: string): void {
+    const link = this.metaService.getTag('rel="canonical"');
+    if (!link) {
+      this.metaService.addTag({ rel: 'canonical', href: url });
+    } else {
+      this.metaService.updateTag({ rel: 'canonical', href: url });
     }
+  }
 
-    private updateTitle(title: string): void {
-        this.titleService.setTitle(title);
-    }
-
-    private updateMetaTags(metaTags: { name: string, content: string }[]): void {
-        metaTags.forEach(tag => {
-            this.metaService.updateTag({ name: tag.name, content: tag.content });
-        });
-    }
+  private updateMetaTags(metaTags: { name: string; content: string }[]): void {
+    metaTags.forEach((tag) => {
+      this.metaService.updateTag({ name: tag.name, content: tag.content });
+    });
+  }
 }
